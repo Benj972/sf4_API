e3a56aa9fdf9b354cb30566eb85d1fdfa7ee8b5b
Benj972 <ben.gallot972@gmail.com>
2018-04-10 09:57:25 +0200

    serialize Product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,18 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Product;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\View;
 
 class ProductController extends Controller
 {
-    /**
-     * @Route("/product", name="product")
+     /**
+     * @Get(
+     *     path = "/products/{id}",
+     *     name = "app_product_show",
+     *     requirements = {"id"="\d+"}
+     * )
+     * @View
      */
-    public function index()
+    public function showAction(Product $product)
     {
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
+    	return $product;
     }
 }
