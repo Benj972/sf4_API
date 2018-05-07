db29ec6cddff9181bbc5a64b38b2277230a56143
Benj972 <ben.gallot972@gmail.com>
2018-05-07 12:03:04 +0200

    correction error

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -3,25 +3,13 @@
 namespace App\Repository;
 
 use App\Entity\Product;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
 
-class ProductRepository extends AbstractRepository
+class ProductRepository extends ServiceEntityRepository
 {
-    //Test Pagination with PagerFanta
-    /*public function search($term, $order = 'asc', $limit = 20, $offset = 0)
+    public function __construct(RegistryInterface $registry)
     {
-        $qb = $this
-            ->createQueryBuilder('p')
-            ->select('p')
-            ->orderBy('p.name', $order)
-        ;
-        
-        if ($term) {
-            $qb
-                ->where('p.name LIKE ?1')
-                ->setParameter(1, '%'.$term.'%')
-            ;
-        }
-        
-        return $this->paginate($qb, $limit, $offset);
-    }*/
+        parent::__construct($registry, Product::class);
+    }
 }
