e0e8e7b2cbfe87c18f3fdd3ad52dd0daa0a3df68
Benj972 <ben.gallot972@gmail.com>
2018-05-28 16:58:46 +0200

    add variable in the route app_product_list

--- src/Handler/PaginateProductsHandler.php
+++ src/Handler/PaginateProductsHandler.php
@@ -6,6 +6,8 @@ use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Hateoas\Representation\PaginatedRepresentation;
 use Hateoas\Representation\CollectionRepresentation;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class PaginateProductsHandler
 {
@@ -14,19 +16,30 @@ class PaginateProductsHandler
      */
     private $manager;
 
+     /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
     /**
      * PaginateProductsHandler constructor.
      * @param EntityManagerInterface $manager
      */
-    public function __construct(EntityManagerInterface $manager)
+    public function __construct(EntityManagerInterface $manager, RequestStack $requestStack)
     {
     	$this->manager = $manager;
+        $this->requestStack = $requestStack; 
     }
 
     public function handle()
     {
-        $products = $this->manager->getRepository(Product::class)->findAll();
-        $paginatedCollection = new PaginatedRepresentation(
+        $request = $this->requestStack->getCurrentRequest();
+        $path= $request->getPathInfo();
+
+        if (in_array($path, array('', '/products'))) {
+                
+            $products = $this->manager->getRepository(Product::class)->findAll();
+            $paginatedCollection = new PaginatedRepresentation(
             new CollectionRepresentation(
                 $products,
                 'products',
@@ -37,8 +50,20 @@ class PaginateProductsHandler
             1,       // page number
             20,      // limit
             4       // total pages
-        );
+            );
 
-        return $paginatedCollection;
+            return $paginatedCollection;
+
+        } elseif('/products/{name} === $path') {
+
+            $name = $_GET["name"];
+            $product = $this->manager->getRepository(Product::class)->getProduct($name);
+            if($product != null){
+                return $product;
+            }
+            else {
+                throw new BadRequestHttpException('Aucun smartphone ne correspond à votre saisi');
+            }
+        }
     }
 }
